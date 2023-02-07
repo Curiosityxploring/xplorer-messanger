f5cfa865acaa50727afd722b6a7a77eb51128393
Godfrey Lebo <emorylebo@gmail.com>
2023-02-07 19:43:15 +0100

    UPDATE HomePage

--- .history/app/page_20230207194250.tsx
+++ .history/app/page_20230207194250.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 
 function HomePage() {
-  return (
-    <div>
-      <h1>Xplorer Messenger</h1>
-    </div>
-  );
+  return <div>
+    <h1>ChatGP</h1>
+  </div>;
 }
 
 export default HomePage;
