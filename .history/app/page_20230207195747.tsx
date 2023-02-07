da7804ddf42efa07d3b0b862b4413e049c9ce318
Godfrey Lebo <emorylebo@gmail.com>
2023-02-07 19:58:48 +0100

    Page build

--- .history/app/page_20230207195747.tsx
+++ .history/app/page_20230207195747.tsx
@@ -2,9 +2,8 @@ import React from "react";
 
 function HomePage() {
   return (
-    <div className="flex flex-col items-center justify-center px-2 h-screen text-white">
+    <div className="flex flex-col items-center justify-center  text-white">
       <h1 className="text-5xl font-bold mb-20">Xplorer Messenger</h1>
-      <div></div>
     </div>
   );
 }
