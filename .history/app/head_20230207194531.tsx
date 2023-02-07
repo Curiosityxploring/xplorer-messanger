8c78667d8a9037b43ac54a4202cf85657e3bd905
Godfrey Lebo <emorylebo@gmail.com>
2023-02-07 19:47:25 +0100

    ADD tailwind

--- .history/app/head_20230207194531.tsx
+++ .history/app/head_20230207194531.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title>Xplorer Messenger - Basically Zita's brain by Godfrey Lebo</title>
+      <title>Xplorer Messenger </title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
     </>
-  );
+  )
 }
--- .history/app/head_20230207194539.tsx
+++ .history/app/head_20230207194539.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title>Xplorer Messenger - Basically Zita's brain by Godfrey Lebo</title>
+      <title>Xplorer Messenger - Basically ZIta</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
     </>
-  );
+  )
 }
