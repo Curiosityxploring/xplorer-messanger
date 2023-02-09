807904b918e920ca67208f46e190d5b324f8631d
Godfrey Lebo <emorylebo@gmail.com>
2023-02-09 20:06:18 +0100

    UPDATE New Chat

--- .history/components/NewChat_20230209200042.tsx
+++ .history/components/NewChat_20230209200042.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function NewChat() {
   return (
-    <div className="border-gray-700 border flex chatRow">
+    <div className="border-gray-700">
       <PlusIcon className="h-4 w-4" />
       <p>New Chat</p>
     </div>
