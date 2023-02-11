82112bcfb9804472439e1be4f6c2622a1e135135
Godfrey Lebo <emorylebo@gmail.com>
2023-02-11 21:33:23 +0100

    UPDATE Login

--- .history/next.config_20230211213238.js
+++ .history/next.config_20230211213238.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  images: {
-    domains: ["links.papareact.com"],
+  images:{
+    
   },
   experimental: {
     appDir: true,
